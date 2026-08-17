26e15e9ac43df746f104488b403262ca0eaf46c9
Stefan Dios Mayarin <116821916+stefanmdev@users.noreply.github.com>
2026-08-17 16:54:26 -0300

    ejercicio 5: suma de numeros

---
+++ tp1-variables-condicionales-js/js/05-suma-de-numeros.js
@@ -0,0 +1,3 @@
+const numero1 = parseInt(prompt("Ingrese el primer numero"))
+const numero2 = parseInt(prompt("Ingrese el segundo numero"))
+console.log("La suma de los números ingresados es: " + (numero1 + numero2))
