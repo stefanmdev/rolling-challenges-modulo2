a05a95edab9bd0475ecb0722986086ea3d0b3a2b
Stefan Dios Mayarin <116821916+stefanmdev@users.noreply.github.com>
2026-08-17 17:16:18 -0300

    ejercicio 6: numero mayor

---
+++ tp1-variables-condicionales-js/js/06-mayor.js
@@ -0,0 +1,8 @@
+const numero1 = parseInt(prompt("Ingrese el primer numero"))
+const numero2 = parseInt(prompt("Ingrese el segundo numero"))
+
+if (numero1 > numero2) {
+  alert("El " + numero1 + " es el número más grande")
+} else {
+  alert("El " + numero2 + " es el número más grande")
+}
