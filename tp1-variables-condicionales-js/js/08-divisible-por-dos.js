605f2a70c1235aebb78136a1e15091f38bafccf4
Stefan Dios Mayarin <116821916+stefanmdev@users.noreply.github.com>
2026-08-17 17:53:59 -0300

    ejercicio 8: divisible por 2

---
+++ tp1-variables-condicionales-js/js/08-divisible-por-dos.js
@@ -0,0 +1,7 @@
+const numero = parseInt(prompt("Ingrese un número"))
+
+if (numero % 2 === 0) {
+  console.log("El " + numero + " es divisible por 2")
+} else {
+  console.log("El " + numero + " no es divisible por 2")
+}
