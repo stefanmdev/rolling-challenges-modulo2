98573f0bc223d932d5adc2ce8bb75b7d5655ecd4
Stefan Dios Mayarin <116821916+stefanmdev@users.noreply.github.com>
2026-08-18 02:28:49 -0300

    ejercicio 9: vocales de una frase

---
+++ tp1-variables-condicionales-js/js/09-vocales-frase.js
@@ -0,0 +1,45 @@
+const frase = prompt("Ingresa una frase")
+const fraseMinus = frase.toLowerCase()
+let resultado = ""
+
+if (fraseMinus.charAt(0) === "a" || fraseMinus.charAt(0) === "e" || fraseMinus.charAt(0) === "i" || fraseMinus.charAt(0) === "o" || fraseMinus.charAt(0) === "u") {
+  resultado = resultado + fraseMinus.charAt(0)
+}
+
+if (fraseMinus.charAt(1) === "a" || fraseMinus.charAt(1) === "e" || fraseMinus.charAt(1) === "i" || fraseMinus.charAt(1) === "o" || fraseMinus.charAt(1) === "u") {
+  resultado = resultado + fraseMinus.charAt(1)
+}
+
+if (fraseMinus.charAt(2) === "a" || fraseMinus.charAt(2) === "e" || fraseMinus.charAt(2) === "i" || fraseMinus.charAt(2) === "o" || fraseMinus.charAt(2) === "u") {
+  resultado = resultado + fraseMinus.charAt(2)
+}
+
+if (fraseMinus.charAt(3) === "a" || fraseMinus.charAt(3) === "e" || fraseMinus.charAt(3) === "i" || fraseMinus.charAt(3) === "o" || fraseMinus.charAt(3) === "u") {
+  resultado = resultado + fraseMinus.charAt(3)
+}
+
+if (fraseMinus.charAt(4) === "a" || fraseMinus.charAt(4) === "e" || fraseMinus.charAt(4) === "i" || fraseMinus.charAt(4) === "o" || fraseMinus.charAt(4) === "u") {
+  resultado = resultado + fraseMinus.charAt(4)
+}
+
+if (fraseMinus.charAt(5) === "a" || fraseMinus.charAt(5) === "e" || fraseMinus.charAt(5) === "i" || fraseMinus.charAt(5) === "o" || fraseMinus.charAt(5) === "u") {
+  resultado = resultado + fraseMinus.charAt(5)
+}
+
+if (fraseMinus.charAt(6) === "a" || fraseMinus.charAt(6) === "e" || fraseMinus.charAt(6) === "i" || fraseMinus.charAt(6) === "o" || fraseMinus.charAt(6) === "u") {
+  resultado = resultado + fraseMinus.charAt(6)
+}
+
+if (fraseMinus.charAt(7) === "a" || fraseMinus.charAt(7) === "e" || fraseMinus.charAt(7) === "i" || fraseMinus.charAt(7) === "o" || fraseMinus.charAt(7) === "u") {
+  resultado = resultado + fraseMinus.charAt(7)
+}
+
+if (fraseMinus.charAt(8) === "a" || fraseMinus.charAt(8) === "e" || fraseMinus.charAt(8) === "i" || fraseMinus.charAt(8) === "o" || fraseMinus.charAt(8) === "u") {
+  resultado = resultado + fraseMinus.charAt(8)
+}
+
+if (fraseMinus.charAt(9) === "a" || fraseMinus.charAt(9) === "e" || fraseMinus.charAt(9) === "i" || fraseMinus.charAt(9) === "o" || fraseMinus.charAt(9) === "u") {
+  resultado = resultado + fraseMinus.charAt(9)
+}
+
+console.log(resultado)
