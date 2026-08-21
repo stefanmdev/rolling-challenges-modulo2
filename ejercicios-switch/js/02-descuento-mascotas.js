3d1943b285a5d0c0379f17b32e4fe5c8b7581070
Stefan Dios Mayarin <116821916+stefanmdev@users.noreply.github.com>
2026-08-21 09:16:55 -0300

    ejercicio 2: descuento mascotas

--- ejercicios-switch/js/02-descuento-mascotas.js
+++ ejercicios-switch/js/02-descuento-mascotas.js
@@ -1,8 +1,7 @@
-const esMiembro = prompt("Es miembro de la tienda?").toLowerCase();
+const esMiembro = prompt("¿Es miembro del club de fidelidad? (si/no)?").toLowerCase();
 
 if (esMiembro === "si") {
-  console.log("¿Es miembro del club de fidelidad? (si/no)");
-  const tipoMascota = prompt("1-perros; 2-gatos; 3-aves");
+  const tipoMascota = prompt("Ingresa una opción: 1- perros; 2- gatos; 3- aves");
   switch (tipoMascota) {
     case "1":
       console.log("El descuento es del 15%");
@@ -14,7 +13,7 @@ if (esMiembro === "si") {
       console.log("El descuento es del 5%");
       break;
     default:
-      console.log("El descuento es del 0%");
+      console.log("Ingresaste una opción inválida");
   }
 } else {
   console.log("No tiene descuento (0%)");
